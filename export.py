eff34dcf9c0b44c56bf4d37947d77e35d1cf5107
gurramnaveen <52168186+gurramnaveen@users.noreply.github.com>
2021-08-21 19:32:42 +0530

    Update export.py

--- export.py
+++ export.py
@@ -5,3 +5,21 @@ Created on Sat Aug 21 19:04:18 2021
 @author: nAVIN
 """
 
+class Export:
+    def __init__(self, filePath):
+        self.fileObj = ""
+        try:
+            self.fileObj = open(filePath,"r")
+        except:
+            print("File Not Found!")
+    
+    def toDataBase(self):
+        if(self.fileObj == ""): return
+        for line in self.fileObj:
+            dataList=line.split('|')
+            dataList.pop(0)
+            if(dataList[0] == 'D'):
+                print(dataList)
+            
+e1=Export("./../test.txt")
+e1.toDataBase()
